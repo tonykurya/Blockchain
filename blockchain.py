d6a0ea268d53a23cac6f1c02b5cf72488c5e0a81
tonykurya <tonykurya@gmail.com>
2017-10-10 22:43:02 +0300

    Update endpoints between Public an Admin API

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -10,6 +12,36 @@ class Blockchain(object):
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes,
+            where p is the previous
+         - p is the previous proof, and p' is the new proof
+
+         :param last_proof: <int>
+         :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contains 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
 
     def new_block(self, proof, previous_hash=None):
         """
